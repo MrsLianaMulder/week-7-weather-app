f936172981ce4005aaa1ecb0d505cef6720f8c34
MrsLianaMulder <127157817+MrsLianaMulder@users.noreply.github.com>
2023-04-18 22:38:33 -0400

    began js

---
+++ src/app.js
@@ -0,0 +1,4 @@
+let apiKey = "4039af31b689baf48b0o7t1d44c20851";
+let apiUrl =
+  "https://api.shecodes.io/weather/v1/current?query=Boston&key=4039af31b689baf48b0o7t1d44c20851";
+console.log(apiUrl);
