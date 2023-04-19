2b1536709bcdaffcbef62ad02b377c9a6ef8db80
MrsLianaMulder <127157817+MrsLianaMulder@users.noreply.github.com>
2023-04-19 16:52:16 -0400

    js added for current temperature

--- src/app.js
+++ src/app.js
@@ -1,4 +1,19 @@
+function displayTemperature(response) {
+  console.log(response);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind-speed");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
 let apiKey = "4039af31b689baf48b0o7t1d44c20851";
 let apiUrl =
-  "https://api.shecodes.io/weather/v1/current?query=Boston&key=4039af31b689baf48b0o7t1d44c20851";
-console.log(apiUrl);
+  "https://api.shecodes.io/weather/v1/current?query=Boston&key=4039af31b689baf48b0o7t1d44c20851&units=metric";
+
+axios.get(apiUrl).then(displayTemperature);
